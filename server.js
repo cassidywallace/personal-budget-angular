c6e610120b60cddf4fd519ede8c3683c1245d69d
cassidywallace <cwalla53@uncc.edu>
2020-10-07 18:28:47 -0400

    add CORS

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
+//budget API
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000
 
-app.use('/', express.static ('public'));
+
+app.use(cors());
 
 const budget = require('./myBudget.json');
 console.log('the type of budget is', typeof budget)
 
 console.log(budget);
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+
+
 app.get('/budget', (req, res) => {
     res.json(budget);
 })
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}');
+    console.log('API served at http://localhost:${port}');
 });
 
 
